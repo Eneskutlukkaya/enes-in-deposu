275427f89fafa5ce600c04578db03cecf38fe1f7
Eneskutlukkaya <78257063+Eneskutlukkaya@users.noreply.github.com>
2021-01-30 13:49:00 +0300

    Add files via upload

---
+++ MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class MusteriManager
+    {
+        public  void add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Name+"eklendi");
+           
+        }
+        public  void delete (Musteri musteri)
+        {
+            Console.WriteLine(musteri.Name+" silindi");
+        }
+        public  void list( Musteri musteri)
+        {
+            Console.WriteLine("Müşteri bilgileri................................");
+            Console.WriteLine( musteri.Id);
+            Console.WriteLine(musteri.Name);
+            Console.WriteLine(musteri.Job);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1234 ;
+            musteri1.Name = "Nicola Tesla";
+            musteri1.Job = "Bilim insanı";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2345;
+            musteri2.Name = "Albert Einstein";
+            musteri2.Job = "Teorik Fizikçi";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 4321;
+            musteri3.Name = "Bill Gates";
+            musteri3.Job = "İş Adamı";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.add(musteri1);
+            musteriManager.list(musteri2);
+            musteriManager.delete(musteri3);
+
+
+        }
+    }
+}
